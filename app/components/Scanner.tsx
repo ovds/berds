cd984fb77965c7143f83d126fbacb58ad7a007fd
iamdedeepsai <dedeepsai.vadapalli@gmail.com>
2023-11-20 16:45:07 +0800

    compass ability added

--- app/components/Scanner.tsx
+++ app/components/Scanner.tsx
@@ -1,28 +1,28 @@
-import { Html5QrcodeScanner } from "html5-qrcode";
+import {Html5Qrcode, Html5QrcodeScanner} from "html5-qrcode";
 import { useEffect, useState } from "react";
 
-export default function Scanner() {
-    const [scanResult, setScanResult] = useState<string | null>(null);
+export default function Scanner(props: {success: Function}) {
+    let html5QrCode: Html5Qrcode;
+    const qrcodeId = "qr-code-reader";
+
     useEffect(() => {
-        const scanner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: {width: 250, height: 250} }, false);
-        scanner.render(success, error);
+        // Anything in here is fired on component mount.
+        if(!html5QrCode?.getState()){
+            html5QrCode = new Html5Qrcode(qrcodeId);
+            const success = (result: String) => {
+                props.success(result);
+                html5QrCode.stop().then(r => console.log(r)).catch(e => console.log(e));
+            };
 
-        function success(result: string) {
-            setScanResult(result);
-            scanner.clear();
-        }
+            const error = (err: String) => {
+                //console.log(err)
+            }
 
-        function error(err: any) {
-            console.log(err);
+            const config = { fps: 10, qrbox: { width: 250, height: 250 }, aspectRatio: 1};
+
+            html5QrCode.start({ facingMode: "environment" }, config, success, error);
         }
     }, []);
 
-    return (
-        <div>
-            { scanResult
-                ? <h2>{scanResult}</h2>
-                : <div id="reader"></div>
-            }
-        </div>
-    )
+    return <div id={qrcodeId}></div>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,111 @@
 'use client'
 
-import Scanner from "@/app/components/Scanner";
+import {useEffect, useState} from "react";
+import Image from 'next/image';
+import * as geolib from "geolib";
+
+const locations = {
+    "Marbles": {
+        "latitude": "1°17'48\"N",
+        "longitude": "103°45'40\"E"
+    },
+    "Lava": {
+        "latitude": "1°17'53\"N",
+        "longitude": "103°45'43\"E"
+    },
+    "Volleyball": {
+        "latitude": "1º17'52.21\"N",
+        "longitude": "103º45'37.88\"E"
+    },
+    "Freeze": {
+        "latitude": "1º17'54.37\"N",
+        "longitude": "103º45'41.37\"E"
+    },
+    "Plant": {
+        "latitude": "1°17'58\"N",
+        "longitude": "103°45'37\"E"
+    },
+    "Tag": {
+        "latitude": "1°17'56\"N",
+        "longitude": "103°45'38\"E"
+    }
+}
 
 export default function Home() {
+    const [text, setText] = useState('');
+    const [userLocation, setUserLocation] = useState<GeolocationPosition | null>(null);
+    const [userHeading, setUserHeading] = useState(0);
+    const [angle, setAngle] = useState(0);
+    const place = "Marbles";
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            setUserLocation(position);
+        });
+    } else {
+        console.log("Not Available");
+    }
+
+    if (window.DeviceOrientationEvent) {
+        window.addEventListener("deviceorientation", handleOrientation);
+    } else {
+        console.log("DeviceOrientationEvent is not supported");
+    }
+
+    function handleOrientation(event: any) {
+        var alpha;
+        // Check for iOS property
+        if (event.webkitCompassHeading) {
+            alpha = event.webkitCompassHeading;
+        }
+        // non iOS
+        else {
+            alpha = event.alpha;
+        }
+        setUserHeading(alpha);
+    }
+
+    useEffect(() => {
+        if (userLocation != null) {
+            setAngle(getBearing() - userHeading);
+            rotateImage(angle);
+        }
+    }, [userLocation]);
+
+    const getBearing = () => {
+        if (userLocation == null) {
+            return 0;
+        }
+        return geolib.getGreatCircleBearing(
+            {
+                latitude: userLocation.coords.latitude,
+                longitude: userLocation.coords.longitude,
+            },
+            {
+                latitude: locations[place].latitude,
+                longitude: locations[place].longitude,
+            }
+        );
+    };
+
+    const rotateImage = (angle: number) => {
+        const image = document.getElementById("arrow");
+        if (image == null) {
+            return;
+        }
+        image.style.transform = `rotate(${userHeading}deg)`;
+    };
+
     return (
-        <div>
-            <h1>QR Code Scanning in React</h1>
-            <Scanner />
+        <div className={'bg-slate-700 w-screen h-screen pt-10 px-5'}>
+            <h1>{userHeading}</h1>
+            <Image
+                id="arrow"
+                src="/arrow.png"
+                width={200}
+                height={200}
+                className="absolute top-1/2 left-1/3 transform -translate-x-2/3 -translate-y-1/2 transition duration-500 ease-in-out"
+                 alt={'Arrow'}></Image>
         </div>
     )
 }
